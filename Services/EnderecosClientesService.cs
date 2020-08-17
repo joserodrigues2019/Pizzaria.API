4041c450b58be681b3ff92f7f06d6db3871aab07
joserodrigues2018 <rodrigues.santos@gmail.com>
2020-08-17 10:32:44 -0400

    Atualização da API pasta Models

---
+++ Services/EnderecosClientesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzaria.API.Services
+{
+    public class EnderecosClientesService
+    {
+
+    }
+}
